a55f75d00073bd7f26001ab0380aac3332214f4f
ben05allen <ben05allen@hotmail.com>
2023-07-05 10:26:17 +0900

    refreshing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from msal import ConfidentialClientApplication
 
 load_dotenv()
 
-TENANT_ID = os.getenv('TENANT_ID')
+TENANT_ID = os.getenv('RALLY_TENANT_ID')
 APP_ID = os.getenv('PBI_CLIENT_ID')
 APP_SECRET = os.getenv('PBI_CLIENT_SECRET')
 GROUP_ID = os.getenv('GROUP_ID')
@@ -24,8 +24,7 @@ if 'access_token' not in result:
     print('No access token')
     exit(1)
 access_token = result['access_token']
-
-print(access_token)
+# print(access_token)
 
 headers = {
     'Authorization': f'Bearer {access_token}',
@@ -42,10 +41,9 @@ for group in groups.json()['value']:
     datasets = requests.get(f"https://api.powerbi.com/v1.0/myorg/groups/{gid}/datasets", headers=headers)
     for dataset in datasets.json()['value']:
         # print(f'\t{dataset.get("name")}')
-        if dataset.get("name") == 'Test':
-            print(dataset_id := dataset.get("id"))
+        # if dataset.get("name") == 'ARL Balanced Scorecard':
+        if dataset.get("name") == 'Rally Xero':
+            print(dataset['name'], dataset_id := dataset.get("id"))
             refresh = requests.post( f'https://api.powerbi.com/v1.0/myorg/groups/{gid}/datasets/{dataset_id}/refreshes', headers=headers)
             print(refresh.status_code)
             print(refresh.text)
-        
-    
